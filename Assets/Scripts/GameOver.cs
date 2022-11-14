16ddc49877aa90adb358af95ad6f572955bbb0b2
DESKTOP-FIEGL49\Owner <20217053-okadahidehito@hcs.ac.jp>
2022-11-14 14:34:36 +0900

    ゲームスタート画面を追加

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField,Header("最初に選択されるボタンを設定")]
+    private Button button;
+
+    [SerializeField, Header("最小歩数を入れるテキストを設定")]
+    private Text text_Step_min;
+
+    //
+    private int minSteps;
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+    public void End()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    private void Awake()
+    {
+        button.Select();
+        minSteps = PlayerPrefs.GetInt("minSteps");
+        text_Step_min.text = string.Format("{0}", minSteps);
+    }
+}
---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+    [SerializeField,Header("最初に選択されるボタンを設定")]
+    private Button button;
+
+    //ゲームを開始する処理
+    public void Play()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    //ゲームを終了する処理
+    public void End()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    /* ゴールまでの最小歩数を初期化
+     * 
+     */
+    private void Awake()
+    {
+        PlayerPrefs.SetInt("min_Steps", 99);
+        PlayerPrefs.Save();
+        button.Select();
+    }
+}
